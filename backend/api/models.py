563b27054339c26b4fbc841946fec2fb86e4a40c
sanyaswee <kozoriz407@gmail.com>
2025-11-08 14:21:30 +0200

    Project model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    city = models.CharField(max_length=100)
+    status = models.CharField(max_length=32)
+    location = models.CharField(max_length=256)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
