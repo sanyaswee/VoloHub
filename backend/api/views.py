c317037881a261729d68ff3c23fae71b4fb6863c
sanyaswee <kozoriz407@gmail.com>
2025-11-08 12:59:39 +0200

    CSRF cookie

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,4 @@
+from django.views.decorators.csrf import ensure_csrf_cookie
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
@@ -5,6 +6,7 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 # Create your views here.
 
 
+@ensure_csrf_cookie
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def ping(request):
